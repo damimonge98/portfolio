21d1720726083626c92d3e51f0cb6eb8f8e4a9e1
Damian <damian@codev.com.ar>
2025-03-01 18:09:48 -0300

    changes

--- autocar-detailing/src/sections/Banner.js
+++ autocar-detailing/src/sections/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../css/Banner.css';
 
+
 const Banner = () => {
     const [isResponsive, setIsResponsive] = useState(false);
 
@@ -16,30 +17,14 @@ const Banner = () => {
         // Llamar la función inicialmente para establecer el estado correcto
         handleResize();
 
-        // Precargar ambas imágenes para mejorar el LCP
-        const preloadImageResponsive = document.createElement('link');
-        preloadImageResponsive.rel = 'preload';
-        preloadImageResponsive.as = 'image';
-        preloadImageResponsive.href = `${process.env.PUBLIC_URL}/img/banner_responsive.webp`;
-        document.head.appendChild(preloadImageResponsive);
-
-        const preloadImageDesktop = document.createElement('link');
-        preloadImageDesktop.rel = 'preload';
-        preloadImageDesktop.as = 'image';
-        preloadImageDesktop.href = `${process.env.PUBLIC_URL}/img/banner.webp`;
-        document.head.appendChild(preloadImageDesktop);
-
-        // Limpiar el listener y eliminar los preload al desmontar el componente
-        return () => {
-            window.removeEventListener('resize', handleResize);
-            document.head.removeChild(preloadImageResponsive); // Eliminar el preload responsive
-            document.head.removeChild(preloadImageDesktop);   // Eliminar el preload desktop
-        };
+        // Limpiar el listener cuando se desmonte el componente
+        return () => window.removeEventListener('resize', handleResize);
     }, []);
 
     return (
         <div className="banner">
             <img
+                rel="preload"
                 src={
                     isResponsive
                         ? `${process.env.PUBLIC_URL}/img/banner_responsive.webp`

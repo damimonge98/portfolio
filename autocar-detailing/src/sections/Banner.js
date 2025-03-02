1f48537453b948acee5971791cdd48546d5cad9d
Damian <damian@codev.com.ar>
2025-03-01 23:36:31 -0300

    cambios tamaño banner

--- autocar-detailing/src/sections/Banner.js
+++ autocar-detailing/src/sections/Banner.js
@@ -6,14 +6,20 @@ const Banner = () => {
     return (
         <div className="banner">
             <img
+            rel="preload"
             className="banner-desktop"
             src={`${process.env.PUBLIC_URL}/img/banner.webp`}
             alt="Banner"
+            fetchpriority="high"
+                loading="eager"
         />
         <img
+            rel="preload"
             className="banner-responsive"
             src={`${process.env.PUBLIC_URL}/img/banner_responsive.webp`}
             alt="Banner"
+            fetchpriority="high"
+                loading="eager"
         />
     </div>
     
